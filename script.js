03c177056434405920540ad5ae3e4a9ab07275c2
Ty Tia <tytia77@gmail.com>
2017-04-09 18:44:23 +1000

    'added'

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const app = express();
 
 // app.use(bodyParser.json());
 
+// set port
+var port = process.env.PORT || 5000;
+app.use(express.static(__dirname));
+
 app.use('/users', require('./routes/users'));
 app.use('/party', require('./routes/party'));
 
-
-
-app.listen(5000, function(){
+app.listen(port, function(){
     console.log('now listening');
 });
